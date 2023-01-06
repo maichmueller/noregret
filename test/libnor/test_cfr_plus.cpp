00a8d56b02612c7e1c5c515efdd16b20c1b38109
Michael <m.aichmueller@gmail.com>
2023-01-06 18:30:15 +0100

    rm stratego test cases

--- test/libnor/test_cfr_plus.cpp
+++ test/libnor/test_cfr_plus.cpp
@@ -19,29 +19,3 @@ TEST(RockPaperScissors, CFR_PLUS)
 {
    run_cfr_on_rps< rm::CFRPlusConfig{} >();
 }
-
-// TEST_F(StrategoState3x3, cfr_vanilla)
-//{
-//    //      auto env = std::make_shared< Environment >(std::make_unique< Logic >());
-//    games::stratego::Environment env{std::make_unique< games::stratego::Logic >()};
-//    UniformPolicy uniform_policy = factory::make_uniform_policy<
-//       games::stratego::InfoState,
-//       HashmapActionPolicy< games::stratego::Action > >();
-//
-//    auto tabular_policy = factory::make_tabular_policy(
-//       std::unordered_map<
-//          games::stratego::InfoState,
-//          HashmapActionPolicy< games::stratego::Action > >{});
-//
-//    constexpr rm::CFRConfig cfr_config{.update_mode = rm::UpdateMode::alternating};
-//
-//    auto solver = factory::make_cfr_vanilla< cfr_config, true >(
-//       std::move(env),
-//       std::make_unique< State >(std::move(state)),
-//       tabular_policy,
-//       std::move(uniform_policy));
-//    solver.initialize();
-//    const auto& curr_policy = *solver.iterate(100);
-//    std::cout << curr_policy.at(Player::alex).table().size();
-//    LOGD2("Table size", curr_policy.at(Player::alex).table().size());
-// }
