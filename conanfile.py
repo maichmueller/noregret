b1385ccf9b4a6476f83dd97b9f095552752b9e55
Michael Aichmueller <m.aichmueller@gmail.com>
2024-07-16 11:11:34 +0200

    fix conan dependencies

--- conanfile.py
+++ conanfile.py
@@ -4,7 +4,7 @@
 from conan import ConanFile
 from conan.tools.cmake import cmake_layout, CMakeToolchain
 
-class ConanApplication(ConanFile):
+class NoRegret(ConanFile):
     package_type = "application"
     settings = "os", "compiler", "build_type", "arch"
     generators = "CMakeDeps"
